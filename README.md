641fca9a71bbdac5323ae22dcebc578a64a72833
Erin Dante <83080103+voidnire@users.noreply.github.com>
2025-07-08 01:37:49 +0000

    starting new instances

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # sudoku-killer
+
+## DATA: 
+- vetor contendo gaiolas e somas respectivas
+([(célula x), (célula y),..., (célula z)], soma))
+[
+    ([(1,1),(1,2)],15),
--- sudokukillerMIP.py
+++ sudokukillerMIP.py
@@ -1,6 +1,6 @@
 import gurobipy as gp
 
-data_killer = [
+og_data_killer = [
     ([(1,1),(1,2)],15),
     ([(2,1),(2,2)],8),
     ([(3,1),(3,2)],3),
@@ -33,16 +33,41 @@ data_killer = [
     ([(8,9),(9,9)],9),
 ]
 
+test_data_1 = [
+    #BLOCO 1
+    ([(1,1),(1,2)],15), #coluna / linha
+    ([(2,1)],1),
+    ([(2,2)],8),
+    ([(1,3),(2,3)],7), #coluna / linha
+    ([(3,1),(4,1),(5,1)],12), #coluna / linha
+    ([(3,2),(4,2)],12), #coluna / linha
+    ([(3,3),(4,3),(4,4)],8), #coluna / linha
+    #BLOCO 1
+
+    #BLOCO 2
+    ([(5,2),(5,3),(5,4)],16),
+    ([(6,1),(7,1),(8,1),(7,2)],18),
+    ([(6,2),(6,3),(7,3)],16),
+    #BLOCO 2
+
+    #BLOCO 3
+    ([(9,1)],8),
+    #BLOCO 3
+
+]
+
+
+
 # checando se ta certo as cages ---------------------------------
-for squares, sum_ in data_killer[6:9]:
-    print(f"Sum of cells marked below must add up to {sum_}")
-    out = ""
-    for x in range(1,10):
-        for y in range(1,10):
-            out += "x" if (y,x) in squares else "."
-        out += "\n"
-    print(out)
-    print()
+#for squares, sum_ in data_killer[6:9]:
+#    print(f"Sum of cells marked below must add up to {sum_}")
+#    out = ""
+#    for x in range(1,10):
+#        for y in range(1,10):
+#            out += "x" if (y,x) in squares else "."
+#        out += "\n"
+#    print(out)
+#    print()
 # checando se ta certo as cages ---------------------------------
 
 # checando se cada celula esta em exatamente 1 grupo ------------------------
@@ -65,17 +90,45 @@ for q in range(1,10):
     data_sudoku.append([(w,q) for w in range(10)])
 
 # checando a regra
-for squares in data_sudoku[:2]:
-    out = ""
-    for x in range(1,10):
-        for y in range(1,10):
-            out += "x" if (y,x) in squares else "."
-        out += "\n"
-    print(out)
-    print()
+#for squares in data_sudoku[:2]:
+#    out = ""
+#    for x in range(1,10):
+#        for y in range(1,10):
+#            out += "x" if (y,x) in squares else "."
+#        out += "\n"
+#    print(out)
+#    print()
 
 # estabelecendo regra de cada grupo contém os números de 1 a 9 exatamente 1 vez ----------------
 
 
 # CONSTRUINDO MODELO MIP CONSTRUINDO MODELO MIP CONSTRUINDO MODELO MIP
 m = gp.Model()
+
+grid = [[[m.addVar(vtype="b", name=f"{x},{y}={i}") for i in range(1,10)] for x in range(1,10)] for y in range(1,10)]
+
+for cell_x in range(1,10):
+    for cell_y in range(1,10):
+        m.addConstr(sum(grid[cell_x-1][cell_y-1]) == 1.)
+
+for cells in data_sudoku:
+    for i in range(1,10):
+        m.addConstr(sum(grid[x-1][y-1][i-1] for (x,y) in cells) >= 1.)
+
+for cells, sum_ in data_killer:
+    m.addConstr(sum(i*grid[x-1][y-1][i-1] for i in range(1,10) for (x,y) in cells) == sum_)
+
+m.optimize() 
+
+
+#PRINTANDO A SOLUÇÃO
+out = ""
+for y in range(1,10):
+    for x in range(1,10):
+        for i in range(1,10):
+            if grid[x-1][y-1][i-1].X >= 0.5:
+                out += " " + str(i)
+        if x in [3, 6]: out += " |"
+    if y in [3, 6]: out += "\n-------+-------+-------"
+    out += "\n"
+print(out)
